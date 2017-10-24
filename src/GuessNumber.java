68c42a6398d9041fc0c79fd74353dd47e9676f54
Dawid Paluszkiewicz <dawid.paluszkiewicz88@gmail.com>
2017-10-24 15:44:20 +0200

    Dodałem wyjątek w przypadku użycia znaku innego niż cyfra

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -10,23 +10,28 @@ public class GuessNumber {
         int numberOfGuesses = 1;
         Random random = new Random();
         int number = random.nextInt(10);
-        while(!result)
-        {
-            System.out.println("Wprowadź liczbę z zakresu 0-10: ");
-            int guess = Integer.parseInt(reader.readLine());
-            if(guess == number) {
-                System.out.println("Znalazłeś szukaną liczbę!!!");
-                result = true;
-            }else if (guess > number) {
-                System.out.println("Podałeś większą liczbę.");
-                result = false;
-                numberOfGuesses++;
-            }else if (guess < number){
-                System.out.println("Podałeś mniejszą liczbę.");
-                result = false;
-                numberOfGuesses++;
+        try{
+            while(!result)
+            {
+                System.out.println("Wprowadź liczbę z zakresu 0-10: ");
+                int guess = Integer.parseInt(reader.readLine());
+                if(guess == number) {
+                    System.out.println("Znalazłeś szukaną liczbę!!!");
+                    result = true;
+                }else if (guess > number) {
+                    System.out.println("Podałeś większą liczbę.");
+                    result = false;
+                    numberOfGuesses++;
+                }else if (guess < number){
+                    System.out.println("Podałeś mniejszą liczbę.");
+                    result = false;
+                    numberOfGuesses++;
+                }
             }
+            System.out.println("Zgadłeś za " + numberOfGuesses + " razem.");
+
+        }catch(NumberFormatException e){
+            System.out.println("Upps podałeś jakiś innych znak. W grze można korzystać tylko z cyfr.");
         }
-        System.out.println("Zgadłeś za " + numberOfGuesses + " razem.");
     }
 }
